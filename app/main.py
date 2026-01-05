212bb388c661a673380d57e80f59c4339cfac644
ArtemZakhar <artemzakharchuk@gmail.com>
2026-01-05 08:42:21 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -15,23 +15,22 @@ class Ship:
         # Create decks and save them to a list `self.decks`
         self.decks = []
         self.is_drowned = is_drowned
-        x_axis = end[0] - start[0]
-        y_axis = end[1] - start[1]
+        min_row = min(start[0], end[0])
+        max_row = max(start[0], end[0])
+        min_column = min(start[1], end[1])
+        max_column = max(start[1], end[1])
 
-        max_axis = max(x_axis, y_axis)
+        is_horizontal = min_row == max_row
+        is_vertical = min_column == max_column
 
-        if start == end:
-            self.decks.append(Deck(*start))
-            return
-
-        for i in range(max_axis):
-            if i == 0:
-                self.decks.append(Deck(start[0], start[1]))
-
-            if x_axis > y_axis:
-                self.decks.append(Deck(start[0] + 1 + i, start[1]))
-            else:
-                self.decks.append(Deck(start[0], start[1] + 1 + i))
+        if is_horizontal:
+            for column in range(min_column, max_column + 1):
+                self.decks.append(Deck(min_row, column))
+        elif is_vertical:
+            for row in range(min_row, max_row + 1):
+                self.decks.append(Deck(row, min_column))
+        else:
+            self.decks.append(Deck(min_row, min_column))
 
     def get_deck(self, row: int, column: int) -> Deck:
         for deck in self.decks:
@@ -42,13 +41,16 @@ class Ship:
         # Change the `is_alive` status of the deck
         # And update the `is_drowned` value if it's needed
         deck = self.get_deck(row, column)
-        deck.is_alive = False
 
-        if any(rest_deck.is_alive for rest_deck in self.decks):
-            return "Hit!"
-        else:
+        if deck:
+            deck.is_alive = False
+
+            if any(rest_deck.is_alive for rest_deck in self.decks):
+                return "Hit!"
+
             self.is_drowned = True
             return "Sunk!"
+        return "Miss!"
 
 
 class Battleship:
@@ -63,57 +65,18 @@ class Battleship:
         # which is located in it
         for ship in ships:
             start, end = ship
-            self.field[ship] = Ship(start, end)
+            ship_obj = Ship(start, end)
+
+            for deck in ship_obj.decks:
+                self.field[(deck.row, deck.column)] = ship_obj
 
     def fire(self, location: tuple[int, int]) -> str:
         # This function should check whether the location
         # is a key in the `self.field`
         # If it is, then it should check if this cell is the last alive
         # in the ship or not.
-        keys = self.field.keys()
-        validated = False
-        ship_key = None
-
-        for key in keys:
-            start_point = None
-            end_point = None
-            value_to_check = None
-
-            start, end = key
-            if (
-                start[0] == end[0] == location[0]
-                and start[1] == end[1] == location[1]
-            ):
-                ship_key = key
-                validated = True
-                break
-
-            if start[0] == end[0] and start[0] == location[0]:
-                start_point = start[1]
-                end_point = end[1]
-                value_to_check = location[1]
-                ship_key = key
-
-            if start[1] == end[1] and start[1] == location[1]:
-                start_point = start[0]
-                end_point = end[0]
-                value_to_check = location[0]
-                ship_key = key
-
-            if (
-                start_point is not None
-                and end_point is not None
-                and value_to_check is not None
-            ):
-                validated = start_point <= value_to_check <= end_point
-                if validated:
-                    break
-
-        if validated:
-            result = self.field[ship_key].fire(*location)
-            if any(ship.is_drowned is False for ship in self.field.values()):
-                return result
-            else:
-                return "You won!"
+        if location in self.field:
+            ship = self.field[location]
+            return ship.fire(*location)
 
         return "Miss!"
